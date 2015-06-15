6270f967959e8cdb9ad3f51efdc9aa46fec607d7
Trevor John <trevor@john.tj>
2015-06-15 17:06:18 -0400

    initial commit

---
+++ app/src/main/java/com/playdraft/hexigonimageview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.playdraft.hexigonimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    ImageView viewById = (ImageView) findViewById(R.id.image);
+    Picasso.with(this)
+      .load("http://sports.cbsimg.net/images/nhl/blog/020214_datsyuk.jpg")
+      .into(viewById);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
