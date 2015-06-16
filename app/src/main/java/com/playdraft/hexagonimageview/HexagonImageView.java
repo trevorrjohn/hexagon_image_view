6f1fbd494d7471b66e180dc90f4f49a0c87ea7ae
Trevor John <trevor@john.tj>
2015-06-16 19:48:55 -0400

    remove unused imports

--- app/src/main/java/com/playdraft/hexagonimageview/HexagonImageView.java
+++ app/src/main/java/com/playdraft/hexagonimageview/HexagonImageView.java
@@ -14,7 +14,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageView;
 
 public class HexagonImageView extends ImageView {
